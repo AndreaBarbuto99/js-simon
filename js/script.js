68f6a74590d0fc8cc2acca8ad8ca814829df50e2
AndreaBarbuto99 <barbutoandrea99@gmail.com>
2025-12-14 03:33:25 +0000

    update script.js

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// dichiarazioni variabili
+let numShow = document.getElementById("numbers-list");
+let formShow = document.getElementById("answers-form")
+let timeNumbers;
+
+
+
+// CHIAMATA FUNZIONI
+numShow.innerHTML = `<li>${getRandomInArray(1, 50, 5)}</li>`
+timeNumbers = setTimeout( () => {
+  numShow.classList.add("bye");
+  formShow.classList.remove("bye");
+}
+, 5000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+formShow.addEventListener("submit", (e) => {
+  e.preventDefault();
+}
+)
+
+function getRandomInArray (min, max, tot) {
+  const numArray = [];
+  
+  while (numArray.length < tot)
+  {
+    const randNum = genRandomNumber(min, max);
+    
+    if (!numArray.includes(randNum))
+    {
+    numArray.push(randNum);
+    }
+  }
+  return numArray;
+}
+
+
+function genRandomNumber(min, max) {
+ return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+console.log(getRandomInArray(1, 50, 5));

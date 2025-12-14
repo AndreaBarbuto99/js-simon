a90013da978682ec0d5a7151ab97cf64ac216910
AndreaBarbuto99 <barbutoandrea99@gmail.com>
2025-12-14 19:28:33 +0100

    feat: added script with console.log of right numbers and which right number

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 // dichiarazioni variabili
 let numShow = document.getElementById("numbers-list");
 let formShow = document.getElementById("answers-form")
+let myButton = document.querySelector(".btn");
+let myForm = document.querySelector(".form-control");
+let userNum = document.querySelectorAll(".user-num");
+let formNum = [];
 let timeNumbers;
+let randArray;
 
 
 
 // CHIAMATA FUNZIONI
-numShow.innerHTML = `<li>${getRandomInArray(1, 50, 5)}</li>`
+randArray = getRandomInArray(1, 50, 5);
+console.log(randArray);
+numShow.innerHTML = `<li>${randArray}</li>`
 timeNumbers = setTimeout( () => {
   numShow.classList.add("d-none");
   formShow.classList.remove("d-none");
   formShow.classList.add("d-block");
 
-  console.log("ciao a tutti")
+ 
 }
-, 3000);
-
-
-
-console.log("ciao a tutti");
+, 5000);
 
 
 
@@ -27,6 +30,37 @@ console.log("ciao a tutti");
 
 
 
+
+let counterUser = 0;
+let userRight = [];
+
+
+
+
+
+myButton.addEventListener("click", () => {
+
+  for (let i = 0; i < 5; i++) {
+    formNum.push(parseInt(userNum[i].value));
+  }
+  console.log(formNum);
+  
+  for (let i = 0; i < 5 ; i++) {
+    
+    if(formNum.includes(randArray[i]))
+    {
+      counterUser++;
+      userRight.push(randArray[i]);
+    }
+    
+  }
+
+  console.log("hai indovinato" + " " + counterUser + " " + "numeri");
+  console.log("i numeri indovinati sono" + " " + userRight);
+
+}
+
+)
 
 
 
@@ -55,4 +89,3 @@ function genRandomNumber(min, max) {
  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-console.log(getRandomInArray(1, 50, 5));
